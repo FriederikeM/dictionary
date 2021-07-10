989e02a4d3c62192f93be3ddbed7bdfbd5936c34
Friederike Moeser <friederike.moeser@gmail.com>
2021-07-10 23:30:36 +0200

    Alert message searched word

--- src/components/Search.js
+++ src/components/Search.js
@@ -1,14 +1,29 @@
+import { useState } from "react";
 import "./Search.css";
 
 export default function Search() {
+  const [searchedWord, setSearchedWord] = useState("");
+
+  function handleSearchChange(event) {
+    const word = event.target.value;
+    setSearchedWord(word);
+  }
+
+  function search(event) {
+    event.preventDefault();
+    alert(`searching ${searchedWord}`);
+  }
   return (
     <header className="Search">
-      <input
-        type="text"
-        placeholder="Search for a word"
-        className="search-input"
-      />
-      <input type="submit" value="GO" className="submit-button" />
+      <form onSubmit={search} className="search-form">
+        <input
+          type="text"
+          placeholder="Search for a word"
+          className="search-input"
+          onChange={handleSearchChange}
+        />
+        <input type="submit" value="GO" className="submit-button" />
+      </form>
     </header>
   );
 }
