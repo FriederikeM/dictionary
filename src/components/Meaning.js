488b0e33105ba1873fda203af1bdf29ed64aad2d
Friederike Moeser <friederike.moeser@gmail.com>
2021-07-17 22:36:45 +0200

    Render synonyms for searched word

--- src/components/Meaning.js
+++ src/components/Meaning.js
@@ -1,7 +1,9 @@
 import "./Meaning.css";
+import Synonyms from "./Synonyms";
 
 export default function Meaning({ meaning }) {
-  console.log(meaning.definitions[0].definition)
+  // console.log(meaning.definitions[0].synonyms
+  //   )
   return(
     <div className="Meaning">
       <h3>{meaning.partOfSpeech}</h3>
@@ -9,11 +11,16 @@ export default function Meaning({ meaning }) {
         return(
           <div key={index}>
             <p>
+              <strong>Definition:</strong>
+              <br />
               {definition.definition}
+              <br />
+              <strong>Example:</strong>
               <br />
               <em>
               {definition.example}
               </em>
+              <Synonyms synonyms={definition.synonyms}/>
             </p>
           </div>
         )
---
+++ src/components/Synonyms.js
@@ -0,0 +1,15 @@
+import "./Synonyms.css"
+
+export default function Synonyms({synonyms}) {
+    if(synonyms) {
+    return (
+        <ul className="Synonyms">
+            {synonyms.map((synonym, index) => {
+               return <li key={index}>{synonym}</li>
+            })}
+        </ul>
+    )}
+    else {
+        return null
+    }
+}
