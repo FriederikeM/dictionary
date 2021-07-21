faa080ecdd8485fd4ffab97b6c5718f3bee89b0a
Friederike Moeser <friederike.moeser@gmail.com>
2021-07-21 16:07:01 +0200

    Style photos and change layout

--- src/components/Meaning.js
+++ src/components/Meaning.js
@@ -7,20 +7,18 @@ export default function Meaning({ meaning }) {
       <h3>{meaning.partOfSpeech}</h3>
       {meaning.definitions.map((definition, index) => {
         return (
-          <div key={index}>
-            <p className="content">
-              <section>
-                <strong>Definition:</strong>
-                <br />
-                {definition.definition}
-              </section>
-              <section>
-                <strong>Example:</strong>
-                <br />
-                <em>{definition.example}</em>
-              </section>
-              <Synonyms synonyms={definition.synonyms} />
-            </p>
+          <div key={index} className="content">
+            <section>
+              <strong>Definition:</strong>
+              <br />
+              {definition.definition}
+            </section>
+            <section>
+              <strong>Example:</strong>
+              <br />
+              <em>{definition.example}</em>
+            </section>
+            <Synonyms synonyms={definition.synonyms} />
           </div>
         );
       })}
--- src/components/Photos.js
+++ src/components/Photos.js
@@ -4,16 +4,18 @@ export default function Photos({ photos, searchedWord }) {
   if (photos) {
     return (
       <div className="Photos">
-        {photos.map((photo, index) => {
-          return (
-            <img
-              src={photo.src.landscape}
-              alt={searchedWord}
-              className="example-image"
-              key={index}
-            />
-          );
-        })}
+        <div className="photos-wrapper">
+          {photos.map((photo, index) => {
+            return (
+              <img
+                src={photo.src.landscape}
+                alt={searchedWord}
+                className="example-image"
+                key={index}
+              />
+            );
+          })}
+        </div>
       </div>
     );
   } else {
